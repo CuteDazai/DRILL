fd84ce36c3c61d78d8307f7af615f7d092866183
cutedazai <mint665@kakao.com>
2021-09-16 17:45:51 +0900

    DRILL #6. 캐릭터 사각및원운동
    2020182039 조명주
    https://youtu.be/Y7hjWMc-y4I

---
+++ 06/character_move.py
@@ -0,0 +1,43 @@
+from pico2d import *
+import math
+open_canvas()
+
+grass =  load_image('grass.png')
+character = load_image('character.png')
+speed = 10
+
+def rendering(x,y):
+    clear_canvas()
+    grass.draw(400,30)
+    character.draw(x,y)
+    update_canvas()
+    delay(0.05)
+
+def move_square(width=800, hight=600):
+    x=20
+    y=90
+    while x<width-20:
+        rendering(x,y)
+        x+=speed
+    while y<hight-40:
+        rendering(x,y)
+        y+=speed
+    while x>20:
+        rendering(x,y)
+        x-=speed
+    while y>90:
+        rendering(x,y)
+        y-=speed
+
+def move_circle():
+    ang=-90;
+    while ang<270:
+        x=math.cos(math.pi*(ang/180))*240+400
+        y=math.sin(math.pi*(ang/180))*240+330
+        rendering(x,y)
+        ang+=speed
+        #print(ang)
+        
+while True:
+    move_circle()
+    move_square()         
